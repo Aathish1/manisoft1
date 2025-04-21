8c7f030c23a8efcd1ca003275034741bc223b003
Aathish1 <iamtheonlyalpha@gmail.com>
2025-04-21 19:27:27 +0530

    Updated files after fixing vite.config and build

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,14 @@ import App from './App.jsx'
 import Page1 from './components/page1.jsx'
 import Page2 from './components/page2.jsx'
 import Error from './components/Error.jsx'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <Router>
       <Routes>
-        <Route path="/Business" element={<App />} />
-        <Route path="/Business/1" element={<Page1 />} />
-        <Route path="/Business/2" element={<Page2 />} />
+        <Route path="/" element={<App />} />
+        <Route path="/1" element={<Page1 />} />
+        <Route path="/2" element={<Page2 />} />
         <Route path="" element={<Error />} />
         <Route path="*" element={<Error />} />
       </Routes>
